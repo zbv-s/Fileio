6b3ec89b071babd98430d9f86418d1cd5dc5371a
zbv-s <aleksandra.jargalova@gmail.com>
2024-08-07 16:37:50 +0800

    Добавлено автоматическое копирование ссылки в буфер обмена

--- load_json.py
+++ load_json.py
@@ -3,6 +3,7 @@ from tkinter import filedialog as fd
 from tkinter import messagebox as mb
 from tkinter import ttk
 import requests
+import pyperclip
 
 
 def upload():
@@ -16,6 +17,9 @@ def upload():
                 link = response.json()['link']
                 entry.delete(0, END)
                 entry.insert(0, link)
+                pyperclip.copy(link)
+                mb.showinfo("Ссылка скопирована", f"Ссылка {link} успешно скопирована в буфер обмена")
+
     except Exception as e:
         mb.showerror("Ошибка", f"Произошла ошибка: {e}")
 
