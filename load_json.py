5dccb7237d7f875531a1afaca3accdb2fd27b2d4
zbv-s <aleksandra.jargalova@gmail.com>
2024-08-07 16:29:02 +0800

    добавлена обработка исключения

--- load_json.py
+++ load_json.py
@@ -1,17 +1,24 @@
 from tkinter import *
 from tkinter import filedialog as fd
+from tkinter import messagebox as mb
 from tkinter import ttk
 import requests
 
 
 def upload():
-    filepath = fd.askopenfilename()
-    if filepath:
-        files = {'file': open(filepath, 'rb')}
-        response = requests.post('https://file.io', files=files)
-        if response.status_code == 200:
-            link = response.json()['link']
-            entry.insert(0, link)
+    try:
+        filepath = fd.askopenfilename()
+        if filepath:
+            with open(filepath, 'rb') as f:
+                files = {'file': f}
+                response = requests.post('https://file.io', files=files)
+                response.raise_for_status()
+                link = response.json()['link']
+                entry.delete(0, END)
+                entry.insert(0, link)
+    except Exception as e:
+        mb.showerror("Ошибка", f"Произошла ошибка: {e}")
+
 
 window = Tk()
 window.title("Сохранение файлов в облако")
